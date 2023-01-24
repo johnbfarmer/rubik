2848d99c9fc374663e44d1b19f786cdf5b0186e5
John Farmer <futiaz@gmail.com>
2023-01-24 15:10:28 -0600

    next iteration

--- Cube.js
+++ Cube.js
@@ -1,44 +1,270 @@
 import React, { Component } from 'react';
 import Face from './Face.js';
 import Tile from './Tile.js';
+import ThreeDCube from './ThreeDCube.js';
 import './App.css';
 const colors = [
-  'red',
   'white',
-  'yellow',
+  'red',
+  'orange',
   'green',
   'blue',
-  'orange'
+  'yellow',
 ];
 
 class Cube extends Component {
+  constructor(props) {
+    super(props);
+    let corners = [
+      this.getCorner([colors[0], colors[3], colors[1],0]),
+      this.getCorner([colors[0], colors[1], colors[4],1]),
+      this.getCorner([colors[0], colors[4], colors[2],2]),
+      this.getCorner([colors[0], colors[2], colors[3],3]),
+      this.getCorner([colors[5], colors[3], colors[1],4]),
+      this.getCorner([colors[5], colors[1], colors[4],5]),
+      this.getCorner([colors[5], colors[4], colors[2],6]),
+      this.getCorner([colors[5], colors[2], colors[3],7]),
+    ];
+
+    this.state = {
+      permutation: [0,1,2,3,4,5,6,7],
+      corners: corners,
+    }
+  }
+
   getCorner = (colors) => {
     return (
       [
-        <Tile key={colors[3]+1} color={colors[0]}/>,
-        <Tile key={colors[3]+2} color={colors[1]}/>,
-        <Tile key={colors[3]+3} color={colors[2]}/>,
+        <Tile key={'_'+colors[3]+0} k={'corner '+colors[3]+', tile '+0} color={colors[0]}/>,
+        <Tile key={'_'+colors[3]+1} k={'corner '+colors[3]+', tile '+1} color={colors[1]}/>,
+        <Tile key={'_'+colors[3]+2} k={'corner '+colors[3]+', tile '+2} color={colors[2]}/>,
       ]
     );
   }
 
-  render() {
-    let corners = [
-      this.getCorner([colors[0], colors[1], colors[4],1]),
-      this.getCorner([colors[0], colors[2], colors[4],2]),
-      this.getCorner([colors[0], colors[2], colors[3],3]),
-      this.getCorner([colors[0], colors[1], colors[4],4]),
-      this.getCorner([colors[5], colors[1], colors[4],5]),
-      this.getCorner([colors[5], colors[2], colors[4],6]),
-      this.getCorner([colors[5], colors[2], colors[3],7]),
-      this.getCorner([colors[5], colors[1], colors[4],8]),
-    ];
+  performMove = (move) => {
+    switch (move) {
+      case 'R':
+        this.rotateCubeLeft();
+        this.rotateTop();
+        this.rotateCubeRight();
+        break;
+      case 'Ri':
+        this.rotateCubeLeft();
+        this.reverseRotateTop();
+        this.rotateCubeRight();
+        break;
+      case 'L':
+        this.rotateCubeRight();
+        this.reverseRotateTop();
+        this.rotateCubeLeft();
+        break;
+      case 'Li':
+        this.rotateCubeRight();
+        this.rotateTop();
+        this.rotateCubeLeft();
+        break;
+      default:
+        alert ('move '+move+' is not defined');
+    }
+  }
 
+  rotateCubeLeft = () => {
+    this.reverseRotateFront();
+    this.reverseRotateBack();
+  }
+
+  rotateCubeRight = () => {
+    this.rotateFront();
+    this.rotateBack();
+  }
+
+  rotateFront = () => {
+    let c = this.state.corners;
+    let tmp = c[0][0];
+    c[0][0] = c[4][1];
+    c[4][1] = c[5][0];
+    c[5][0] = c[1][2];
+    c[1][2] = tmp;
+    tmp = c[1][1];
+    c[1][1] = c[0][2];
+    c[0][2] = c[4][2];
+    c[4][2] = c[5][1];
+    c[5][1] = tmp;
+    tmp = c[1][0];
+    c[1][0] = c[0][1];
+    c[0][1] = c[4][0];
+    c[4][0] = c[5][2];
+    c[5][2] = tmp;
+    this.setState({ corners: c });
+  }
+
+  reverseRotateFront = () => {
+    let c = this.state.corners;
+    let tmp = c[0][0];
+    c[0][0] = c[1][2];
+    c[1][2] = c[5][0];
+    c[5][0] = c[4][1];
+    c[4][1] = tmp;
+    tmp = c[1][1];
+    c[1][1] = c[5][1];
+    c[5][1] = c[4][2];
+    c[4][2] = c[0][2];
+    c[0][2] = tmp;
+    tmp = c[1][0];
+    c[1][0] = c[5][2];
+    c[5][2] = c[4][0];
+    c[4][0] = c[0][1];
+    c[0][1] = tmp;
+    this.setState({ corners: c });
+  }
+
+  rotateBack = () => {
+    let c = this.state.corners;
+    let tmp = c[3][0];
+    c[3][0] = c[7][2];
+    c[7][2] = c[6][0];
+    c[6][0] = c[2][1];
+    c[2][1] = tmp;
+    tmp = c[2][2];
+    c[2][2] = c[3][1];
+    c[3][1] = c[7][1];
+    c[7][1] = c[6][2];
+    c[6][2] = tmp;
+    tmp = c[2][0];
+    c[2][0] = c[3][2];
+    c[3][2] = c[7][0];
+    c[7][0] = c[6][1];
+    c[6][1] = tmp;
+    this.setState({ corners: c });
+  }
+
+  reverseRotateBack = () => {
+    let c = this.state.corners;
+    let tmp = c[3][0];
+    c[3][0] = c[2][1];
+    c[2][1] = c[6][0];
+    c[6][0] = c[7][2];
+    c[7][2] = tmp;
+    tmp = c[2][2];
+    c[2][2] = c[6][2];
+    c[6][2] = c[7][1];
+    c[7][1] = c[3][1];
+    c[3][1] = tmp;
+    tmp = c[2][0];
+    c[2][0] = c[6][1];
+    c[6][1] = c[7][0];
+    c[7][0] = c[3][2];
+    c[3][2] = tmp;
+    this.setState({ corners: c });
+  }
+
+  rotateTop = () => {
+    let c = this.state.corners;
+    let tmp = c[0];
+    c[0] = c[1];
+    c[1] = c[2];
+    c[2] = c[3];
+    c[3] = tmp;
+    this.setState({ corners: c });
+  }
+
+  rotateBottom = () => {
+    let c = this.state.corners;
+    let tmp = c[4];
+    c[4] = c[5];
+    c[5] = c[6];
+    c[6] = c[7];
+    c[7] = tmp;
+    this.setState({ corners: c });
+  }
+
+  reverseRotateTop = () => {
+    let c = this.state.corners;
+    let tmp = c[0];
+    c[0] = c[3];
+    c[3] = c[2];
+    c[2] = c[1];
+    c[1] = tmp;
+    this.setState({ corners: c });
+  }
+
+  reverseRotateBottom = () => {
+    let c = this.state.corners;
+    let tmp = c[4];
+    c[4] = c[7];
+    c[7] = c[6];
+    c[6] = c[5];
+    c[5] = tmp;
+    this.setState({ corners: c });
+  }
+
+  render() {
+    let tilesArranged = [
+      [this.state.corners[3][0],this.state.corners[2][0],this.state.corners[0][0],this.state.corners[1][0]], // top
+      [this.state.corners[0][2],this.state.corners[1][1],this.state.corners[4][2],this.state.corners[5][1]], // front
+      [this.state.corners[7][0],this.state.corners[6][0],this.state.corners[4][0],this.state.corners[5][0]], // bottom
+      [this.state.corners[2][2],this.state.corners[3][1],this.state.corners[6][2],this.state.corners[7][1]], // back
+      [this.state.corners[6][2],this.state.corners[5][1],this.state.corners[2][2],this.state.corners[1][1]], // right
+      [this.state.corners[7][2],this.state.corners[3][2],this.state.corners[4][1],this.state.corners[0][1]], // left
+    ];
+    let faces = tilesArranged.map((ta,i) => {
+      return <Face tiles={ta} key={`f_${i}`} />
+    });
     return (
-        <div className='cube-1'>
-          <Face tiles={[corners[0][0],corners[1][0],corners[2][0],corners[3][0]]} />
-          <Face tiles={[corners[0][1],corners[3][1],corners[4][1],corners[3][1]]} />
-          <Face tiles={[corners[1][1],corners[2][1],corners[5][1],corners[6][1]]} />
+        <div className='main-view'>
+          <div className='cube-views'>
+            <div className='cube-1'>
+              { faces }
+            </div>
+            <div className='cube-2'>
+              < ThreeDCube tiles={ tilesArranged } />
+            </div>
+          </div>
+          <div className='controls'>
+            <div className='pointer' onClick={this.rotateCubeLeft}>
+              rotate cube left
+            </div>
+            <div className='pointer' onClick={this.rotateCubeRight}>
+              rotate cube right
+            </div>
+            <div className='pointer' onClick={() => this.performMove('R')}>
+              R
+            </div>
+            <div className='pointer' onClick={() => this.performMove('Ri')}>
+              Ri
+            </div>
+            <div className='pointer' onClick={() => this.performMove('L')}>
+              L
+            </div>
+            <div className='pointer' onClick={() => this.performMove('Li')}>
+              Li
+            </div>
+            <div className='pointer' onClick={this.rotateTop}>
+              U
+            </div>
+            <div className='pointer' onClick={this.reverseRotateTop}>
+              Ui
+            </div>
+            <div className='pointer' onClick={() => this.rotateBottom()}>
+              D
+            </div>
+            <div className='pointer' onClick={() => this.reverseRotateBottom()}>
+              Di
+            </div>
+            <div className='pointer' onClick={this.rotateFront}>
+              F
+            </div>
+            <div className='pointer' onClick={this.reverseRotateFront}>
+              Fi
+            </div>
+            <div className='pointer' onClick={this.rotateBack}>
+              B
+            </div>
+            <div className='pointer' onClick={this.reverseRotateBack}>
+              Bi
+            </div>
+          </div>
         </div>
     );
   }
--- Tile.js
+++ Tile.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-const colors = [
-  'red',
-  'white',
-  'yellow',
-  'green',
-  'blue',
-  'orange'
-];
+
 class Tile extends Component {
   render() {
-    // let color = colors[Math.floor(6*Math.random())];
     let color = this.props.color;
-    console.log(color);
-    return (
-      
-          <div className='cube-1-tile' style={{backgroundColor:color}}>
-          </div>
+    return (  
+      <div className='cube-1-tile' style={{backgroundColor:color}}>
+        { this.props.k }
+      </div>
     );
   }
 }
